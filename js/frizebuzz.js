aa21defe0e55641c51d53afa71ef0d23089233f8
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-04-29 18:08:03 +0200

    ok solved

--- js/frizebuzz.js
+++ js/frizebuzz.js
@@ -2,17 +2,16 @@
 // ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for (var i = 0; i <= 15; i++ ) {
-    console.log(i);
-    if ( i % 3 == 0 ) {
-        console.log("fizz");
+for (var i = 1; i <= 15; i++ ) {
+    // console.log(i); 
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
      } else if ( i % 5 == 0 ) {
         console.log("buzz");
+     } else if (i % 3 == 0) {
+         console.log("fizz");
+     } else {
+         console.log(i);
      }
 }
 
-if ( i / 3 == 0 ) {
-    i = "fiz"
- } else if ( i / 5 == 0 ) {
-     i = "buzz"
- } 
